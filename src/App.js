29d30f5b829fd389be7697c1ab8eb5320c4577ff
unknown <sifatniloy18@gmail.com>
2022-10-31 08:37:13 -0600

    route path created in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Shared/Header/Header';
+import Footer from './components/Shared/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home/Home';
+import Packages from './components/Home/Packages/Packages';
+import About from './components/About/About';
+import Login from './components/Login/Login/Login';
+import Register from './components/Login/Register/Register';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}>Home</Route>
+        <Route path='/packages' element={<Packages></Packages>}></Route>
+        <Route path='/about' element={<About></About>}>About</Route>
+        <Route path='/login' element={<Login></Login>}>Login</Route>
+        <Route path='/register' element={<Register></Register>}>Register</Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
