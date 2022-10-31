efadb2041f527866784bc5302cd28207214da82c
unknown <sifatniloy18@gmail.com>
2022-10-31 10:02:01 -0600

    bootstrap added

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <h2>This is header</h2>
+            <nav>
+                <Link to='/'>Home</Link>
+            </nav>
         </div>
     );
 };
