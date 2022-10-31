8b0c6728b492d165ac51b48e0c6fb7b7e64a1bbe
unknown <sifatniloy18@gmail.com>
2022-10-31 11:00:54 -0600

    banner img added

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-
+import ballon from '../../../images/banner/ballon.jpg';
+import building from '../../../images/banner/building.jpg';
+import sunset from '../../../images/banner/sunset.jpg'
 const Banner = () => {
     return (
         <Carousel fade>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=First slide&bg=373940"
-                    alt="First slide"
-                />
+                <img src={ballon} alt="" />
                 <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Second slide&bg=282c34"
-                    alt="Second slide"
-                />
+                <img src={sunset} alt="" />
 
                 <Carousel.Caption>
                     <h3>Second slide label</h3>
@@ -28,11 +22,7 @@ const Banner = () => {
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Third slide"
-                />
+                <img src={building} alt="" />
 
                 <Carousel.Caption>
                     <h3>Third slide label</h3>
