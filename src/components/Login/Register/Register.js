71ccbec784a84b1f10940482e81193e7662c7de3
unknown <sifatniloy18@gmail.com>
2022-11-02 03:06:36 -0600

    requireAuth and email pass auth added

--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -1,8 +1,16 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
     const nameRef = useRef('');
     const emailRef = useRef('');
     const passwordRef = useRef('');
@@ -10,12 +18,16 @@ const Register = () => {
     const navigateLogin = () => {
         navigate('/login');
     }
+    if(user){
+        navigate('/home');
+    }
     const handleRegister = event => {
         event.preventDefault();
         const name = nameRef.current.value;
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        console.log(name, email, password)
+        // console.log(name, email, password);
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className='container w-50 mx-auto'>
