173eb0d30fee59d6084b64445e5077fd65df5ffc
SifatNiloy <SifatNiloy18@gmail.com>
2024-03-11 16:26:59 +0600

    offers js code reduced

--- src/components/Home/Offers/Offers.js
+++ src/components/Home/Offers/Offers.js
@@ -1,61 +1,49 @@
-import React from 'react';
-import { Button, Card } from 'react-bootstrap';
-import './Offers.css';
-import backpack from '../../../images/products/backpack.jpg';
-import pillow from '../../../images/products/pillow.jpg';
-import keds from '../../../images/products/keds.jpeg';
-import BudgetTravel from '../../BudgetTravel/BudgetTravel';
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import backpackImage from "../../../images/products/backpack.jpg";
+import kedsImage from "../../../images/products/keds.jpeg";
+import pillowImage from "../../../images/products/pillow.jpg";
+import BudgetTravel from "../../BudgetTravel/BudgetTravel";
+
+import "./Offers.css";
+
+const productData = [
+  { name: "Adventure Backpack", image: backpackImage, price: 30 },
+  { name: "Comfort Pillow", image: kedsImage, price: 25 },
+  { name: "Stylish Keds", image: pillowImage, price: 40 },
+];
 
 const Offers = () => {
   return (
     <div className="offer-page">
       <div className="py-4 container">
-        <h2 className="text-center mt-2 display-4 fw-bold text-primary">Special Offers</h2>
-        <div>
-          <h3 className="ms-5 text-center text-dark">Unlock exclusive discounts for every season!</h3>
-          <div>
-            <h2 className="m-5 text-center text-success">Plan your budget travel with us</h2>
-            <BudgetTravel></BudgetTravel>
-          </div>
+        <h2 className="text-center mt-2 display-4 fw-bold">Special Offers</h2>
+        <div className="exclusive-discount text-center">
+          <h3 className="ms-5">
+            Explore exclusive travel deals for every season
+          </h3>
+          <BudgetTravel />
         </div>
-        <h3 className="my-3 text-center text-warning">Order products from us and enjoy exclusive discounts</h3>
+
+        <h3 className="my-3 text-center">
+          Enjoy special discounts on our selected products
+        </h3>
         <div className="row">
-          <Card className="col-sm-12 col-md-6 col-lg-4 mt-2 offer-card">
-            <Card.Img variant="top" src={backpack} className="offer-image" />
-            <Card.Body>
-              <Card.Title>Stylish Backpack</Card.Title>
-              <Card.Text>
-                <p className="offer-price">Price: $30</p>
-              </Card.Text>
-              <Button variant="primary" className="exclusive-button">
-                Buy Now
-              </Button>
-            </Card.Body>
-          </Card>
-          <Card className="col-sm-12 col-md-6 col-lg-4 mt-2 offer-card">
-            <Card.Img variant="top" src={pillow} className="offer-image" />
-            <Card.Body>
-              <Card.Title>Comfortable Pillow</Card.Title>
-              <Card.Text>
-                <p className="offer-price">Price: $30</p>
-              </Card.Text>
-              <Button variant="primary" className="exclusive-button">
-                Buy Now
-              </Button>
-            </Card.Body>
-          </Card>
-          <Card className="col-sm-12 col-md-6 col-lg-4 mt-2 offer-card">
-            <Card.Img variant="top" src={keds} className="offer-image" />
-            <Card.Body>
-              <Card.Title>Stylish Keds</Card.Title>
-              <Card.Text>
-                <p className="offer-price">Price: $30</p>
-              </Card.Text>
-              <Button variant="primary" className="exclusive-button">
-                Buy Now
-              </Button>
-            </Card.Body>
-          </Card>
+          {productData.map((product, index) => (
+            <Card
+              key={index}
+              className="col-sm-12 col-md-6 col-lg-4 mt-2 card-offer"
+            >
+              <Card.Img variant="top" src={product.image} />
+              <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>
+                  <p className="price">Price: ${product.price}</p>
+                </Card.Text>
+                <Button variant="primary">Buy Now</Button>
+              </Card.Body>
+            </Card>
+          ))}
         </div>
       </div>
     </div>
