862a738cde805d695fc9e370c432c25831ab6f9f
SifatNiloy <SifatNiloy18@gmail.com>
2024-03-11 15:20:49 +0600

    budgetTravel fixed to center

--- src/hooks/usePackages.js
+++ src/hooks/usePackages.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
-const usePackages=()=>{
-    const [packages, setpackages] = useState([]);
-    useEffect(() => {
-        fetch('https://travelofun-server.onrender.com')
-            .then(res => res.json())
-            .then(data => setpackages(data))
-    });
-    return [packages, setpackages];
-}
+const usePackages = () => {
+  const [packages, setpackages] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000")
+      .then((res) => res.json())
+      .then((data) => setpackages(data));
+  });
+  return [packages, setpackages];
+};
 
 export default usePackages;
