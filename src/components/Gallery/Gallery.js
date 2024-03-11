5006686be645764d10d4158360a86e47359a33f8
SifatNiloy <SifatNiloy18@gmail.com>
2024-03-11 15:40:56 +0600

    tourise gallary styled and made code shorter

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import tourist1 from '../../../src/images/gallery/tourist-1.jpg';
-import tourist2 from '../../../src/images/gallery/tourist-2.jpg';
-import tourist3 from '../../../src/images/gallery/tourist-3.jpg';
-import tourist4 from '../../../src/images/gallery/tourist-4.jpg';
-import tourist5 from '../../../src/images/gallery/tourist-5.jpg';
-import tourist6 from '../../../src/images/gallery/tourist-6.jpg';
+import React from "react";
+import tourist1 from "../../../src/images/gallery/tourist-1.jpg";
+import tourist2 from "../../../src/images/gallery/tourist-2.jpg";
+import tourist3 from "../../../src/images/gallery/tourist-3.jpg";
+import tourist4 from "../../../src/images/gallery/tourist-4.jpg";
+import tourist5 from "../../../src/images/gallery/tourist-5.jpg";
+import tourist6 from "../../../src/images/gallery/tourist-6.jpg";
+import "./Gallery.css";
 
-import './Gallery.css';
 const Gallery = () => {
-    return (
-        <div className='container'>
-            <h2 className='text-center my-4 p-3'>GALLERY OF OUR TOURISTS</h2>
-            <div className='row '>
-                <img className='tourist-img col-sm-12 col-md-6 col-lg-4 mb-4' src={tourist1} alt="" />
-                <img className='tourist-img col-sm-12 col-md-6 col-lg-4 mb-4' src={tourist2} alt="" />
-                <img className='tourist-img col-sm-12 col-md-6 col-lg-4 mb-4' src={tourist3} alt="" />
-                <img className='tourist-img col-sm-12 col-md-6 col-lg-4 mb-4' src={tourist4} alt="" />
-                <img className='tourist-img col-sm-12 col-md-6 col-lg-4 mb-4' src={tourist5} alt="" />
-                <img className='tourist-img col-sm-12 col-md-6 col-lg-4 mb-4' src={tourist6} alt="" />
-            </div>
-        </div>
-    );
+  const images = [tourist1, tourist2, tourist3, tourist4, tourist5, tourist6];
+
+  return (
+    <div className="container">
+      <h2 className="text-center my-4 p-3">GALLERY OF OUR TOURISTS</h2>
+      <div className="row">
+        {images.map((image, index) => (
+          <div key={index} className="col-sm-12 col-md-6 col-lg-4 mb-4">
+            <img
+              className="tourist-img img-fluid rounded"
+              src={image}
+              alt={`Tourist ${index + 1}`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,78 +1,112 @@
-import React, { useRef } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import auth from '../../../firebase.init';
-import Loading from '../../Shared/Loading/Loading';
-import SocialLogin from '../SocialLogin/SocialLogin';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './Login.css'
+import React, { useRef } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
+import SocialLogin from "../SocialLogin/SocialLogin";
+import "./Login.css";
+
 const Login = () => {
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const navigate = useNavigate();
-    const location = useLocation();
-    const from = location.state?.from?.pathname || "/";
-    const [
-        signInWithEmailAndPassword,
-        user,
-        loading,
-        error,
-    ] = useSignInWithEmailAndPassword(auth);
-    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
-    if (loading || sending) {
-        return <Loading></Loading>
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+  const [signInWithEmailAndPassword, user, loading, ] =
+    useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+  if (loading || sending) {
+    return <Loading />;
+  }
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password);
+  };
+
+  const navigateRegister = () => {
+    navigate("/register");
+  };
+
+  const resetPassword = async () => {
+    const email = emailRef.current.value;
+    if (email) {
+      await sendPasswordResetEmail(email);
+      toast("Sent email");
+    } else {
+      toast("Please enter your email address");
     }
-    if (user) {
-        navigate(from, { replace: true });
-    }
-    const handleSubmit = event => {
-        event.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        signInWithEmailAndPassword(email, password);
-    }
+  };
 
-    const navigateRegister = event => {
-        navigate('/register');
+  return (
+    <Container className="login-container">
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={6}>
+          <h2 className="login-title text-center mt-5 mb-3">Please Login</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                ref={emailRef}
+                type="email"
+                placeholder="Your email"
+                required
+              />
+            </Form.Group>
 
-    }
-    const resetPassword = async () => {
-        const email = emailRef.current.value;
-        if (email) {
-            await sendPasswordResetEmail(email);
-            toast('Sent email');
-        }
-        else{
-            toast('please enter your email address')
-        }
-    }
-    return (
-        <div className='container w-50 mx-auto'>
-            <h2 className='login-title  text-center mt-5 mb-3'>please login</h2>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control ref={emailRef} type="email" placeholder="Your email" required />
-                </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                ref={passwordRef}
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Button variant="primary w-100" type="submit">
+              Login
+            </Button>
+          </Form>
 
-                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
-                </Form.Group>
+          <p className="mt-2">
+            Don't have an account?{" "}
+            <Link
+              to="/register"
+              className="text-danger pe-auto text-decoration-none"
+              onClick={navigateRegister}
+            >
+              Sign Up
+            </Link>
+          </p>
+          <p>
+            Forgot password?{" "}
+            <Button
+              variant="link"
+              className="text-danger pe-auto text-decoration-none"
+              onClick={resetPassword}
+            >
+              Reset Password
+            </Button>
+          </p>
 
-                <Button variant="primary w-50 mx-auto d-block" type="submit">
-                    Login
-                </Button>
-            </Form>
-
-            <p className='mt-2'>Don't have an accout? <Link to='/register' className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Sign Up</Link></p>
-            <p>forgot password? <button className=' btn btn-link text-danger pe-auto text-decoration-none' onClick={resetPassword}>Reset password</button></p>
-
-            <SocialLogin></SocialLogin>
-            <ToastContainer />
-        </div>
-    );
+          <SocialLogin />
+          <ToastContainer />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Login;
