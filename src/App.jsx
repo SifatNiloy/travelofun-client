93fdf50ba0f593acfa2d44fba0351b79be893992
SifatNiloy <sifatniloy18@gmail.com>
2024-06-22 20:56:02 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
-import Pages from "./component/Pages";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <Pages/>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
